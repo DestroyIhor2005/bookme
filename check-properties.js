33846a726e89d52197c968257b45d923f90cd0f4
Ihor Yakymiv <iihorko08@gmail.com>
2026-05-21 21:37:12 +0300

    Update project

--- check-properties.js
+++ check-properties.js
@@ -1,21 +1,18 @@
+import 'dotenv/config';
 import mongoose from 'mongoose';
-
-const Schema = mongoose.Schema;
-
-const propertySchema = new Schema({}, { strict: false });
-const Property = mongoose.model('Property', propertySchema);
 
 async function check() {
   const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/bookme';
   await mongoose.connect(uri);
-  
-  const count = await Property.countDocuments();
+
+  const propertiesCollection = mongoose.connection.db.collection('Properties');
+  const count = await propertiesCollection.countDocuments();
   console.log('Total properties:', count);
-  
-  const byCity = await Property.aggregate([
+
+  const byCity = await propertiesCollection.aggregate([
     { $group: { _id: '$city', count: { $sum: 1 } } },
     { $sort: { count: -1 } }
-  ]);
+  ]).toArray();
   console.log('\nProperties by city:');
   byCity.forEach(item => console.log(`  ${item._id}: ${item.count}`));
   
